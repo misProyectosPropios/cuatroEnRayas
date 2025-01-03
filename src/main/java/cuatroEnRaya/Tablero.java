ca355a88cf4bdac0f7f53d640072715ea6e431e9
misProyectosPropios <inakifrutos@outlook.com>
2025-01-03 18:08:21 -0300

    MODIFY cuatroEnRaya/Tablero. ADD array numPieces

--- src/main/java/cuatroEnRaya/Tablero.java
+++ src/main/java/cuatroEnRaya/Tablero.java
@@ -4,9 +4,11 @@ public class Tablero {
     final private int columna = 7;
     final private int fila = 6;
     Ficha[][] board = new Ficha[fila][columna];
+    int[] numPieces = new int[columna];
 
     /**
      * Init the array named board to be all null
+     * and init numPieces to be 0
      */
     private void initTablero() {
 
