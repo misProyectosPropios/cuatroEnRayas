7eea9a71a4b93f5ef0779cee2cbd8a27c3774543
misProyectosPropios <inakifrutos@outlook.com>
2025-01-05 18:47:00 -0300

    ADD method to associate each player with a given Ficha

--- src/main/java/cuatroEnRaya/Ficha.java
+++ src/main/java/cuatroEnRaya/Ficha.java
@@ -10,4 +10,10 @@ public enum Ficha {
         if (piece == Ficha.YELLOW) return Ficha.RED;
         return Ficha.NONE;
     }
+
+    public static Ficha associateJugadorToFicha(Jugador jug) {
+        if (jug == Jugador.BLACK) return Ficha.RED;
+        if (jug == Jugador.WHITE) return Ficha.YELLOW;
+        return Ficha.NONE;
+    }
 }
