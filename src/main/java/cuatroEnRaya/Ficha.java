779202dde8275f01acc2b6e4cf8d0dfc344f6824
misProyectosPropios <inakifrutos@outlook.com>
2025-01-05 18:34:45 -0300

    ADD method to Ficha.java to get the oppositeFicha

--- src/main/java/cuatroEnRaya/Ficha.java
+++ src/main/java/cuatroEnRaya/Ficha.java
@@ -1,4 +1,13 @@
 package cuatroEnRaya;
 
 public enum Ficha {
+    YELLOW,
+    RED,
+    NONE;
+
+    public static Ficha fichaOpuesta(Ficha piece) {
+        if (piece == Ficha.RED) return Ficha.YELLOW;
+        if (piece == Ficha.YELLOW) return Ficha.RED;
+        return Ficha.NONE;
+    }
 }
